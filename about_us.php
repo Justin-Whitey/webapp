aa72f2e86afb6a51f5396e2c1ccc999b21ca911f
Justin-Whitey <jbcatulong23-0340@cca.edu.ph>
2025-05-14 16:10:37 +0800

    full code done

--- about_us.php
+++ about_us.php
@@ -8,9 +8,7 @@
     <link rel="stylesheet" type="text/css" href="about.css">
 </head>
 <body>
-    <header>
-        
-    </header>
+    <header></header>
     
     <nav>
         <a href="log_in.php" class="login-button">Log In</a>
@@ -26,7 +24,42 @@
     
     <div class="para-text">
         <p>At PME’X, we specialize in efficient and reliable inventory management tailored specifically for small mammals—rabbits and hamsters. Our system ensures precise tracking of breed types, quantities, and related resources, helping breeders and pet care facilities streamline their operations with accuracy and ease. Backed by data and built for simplicity, PME’X is your trusted partner in managing what matters most.</p>
-
     </div>
+
+    <h2 class="about-heading">About Us</h2>
+
+    <div class="about-paragraph">
+        <p>Welcome user we are PMEX and here are the creators behind this webapp</p>
+    </div>
+    
+    <div class="creator-section">
+    <img src="pics/justin.jpg" alt="Creator - Justin" class="creator-img">
+    <p class="creator-text">Justin is one of the minds behind PME’X. Dedicated to simplicity and precision, he ensures that our inventory system serves small mammal caretakers with modern, effective tools.</p>
+    </div>
+
+    <!-- Andrew -->
+    <div class="creator-section">
+        <img src="pics/ronqs.jpeg" alt="Member 1" class="creator-img">
+        <p class="creator-text">andrew</p>
+    </div>
+
+    <!-- Resty -->
+    <div class="creator-section">
+        <img src="pics/resty.jpeg" alt="Member 2" class="creator-img">
+        <p class="creator-text">resty</p>
+    </div>
+
+    <!-- Kaycee -->
+    <div class="creator-section">
+        <img src="pics/kaycee.jpg" alt="Member 3" class="creator-img">
+        <p class="creator-text">Kaycee</p>
+    </div>
+
+    <!-- Kalbo -->
+    <div class="creator-section">
+        <img src="pics/roman.jpg" alt="Member 4" class="creator-img">
+        <p class="creator-text">roman</p>
+    </div>
+
 </body>
 </html>
